265d4a953a9c54b1cfba261151b112092cceceb5
Dave Abrahams <dabrahams@adobe.com>
2023-11-20 13:21:36 -0800

    comments and formatting

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@
 
 import PackageDescription
 
+// Define a constant to clean up dependency management for SPM bug workarounds (see
+// ResourceGeneratorPlugin below).  Swift only allows conditional compilation at statement
+// granularity so that becomes very inconvenient otherwise.
 #if os(Windows)
 let onWindows = true
 #else
@@ -20,15 +23,16 @@ let package = Package(
 
     .plugin(
       name: "ResourceGeneratorPlugin", capability: .buildTool(),
-      // On Windows the plugin cannot have a dependency on the tool,
-      // or building tests that depend (transitively) on the output of
-      // the plugin fail to build with link errors about duplicate
+      // This plugin depends on the executable GenerateResource tool.
+      //
+      // On Windows the plugin cannot have a dependency on the tool, or building tests that depend
+      // (transitively) on the output of the plugin fail to build with link errors about duplicate
       // main functions
-      // (https://github.com/apple/swift-package-manager/issues/6859#issuecomment-1720371716).
+      // (https://github.com/apple/swift-package-manager/issues/6859#issuecomment-1720371716).  On
+      // other platforms the plugin must have a dependency on the tool.
       //
       dependencies: onWindows ? [] : ["GenerateResource"]
     ),
-
 
     .executableTarget(name: "GenerateResource",
       swiftSettings: [ .unsafeFlags(["-parse-as-library"]) ]),
